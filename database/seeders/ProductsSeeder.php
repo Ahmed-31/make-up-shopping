34f942fd9cb2935313628136a3fb0df9f090082f
Mariam Moaz <mariam.moaz10@gmail.com>
2023-12-25 03:25:24 +0200

    seed products table with products data

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Products;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,33 @@ class ProductsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Sample product data
+        $products = [
+            [
+                'name' => 'Lipstick',
+                'description' => 'Transform your look with a swipe of color using our luxurious lipstick collection. Our highly pigmented formulas offer vibrant shades that glide on smoothly, leaving your lips with a bold and long-lasting finish. Elevate your beauty routine with the perfect lip color for any occasion.',
+                'price' => 14.99,
+            ],
+            [
+                'name' => 'Mascara',
+                'description' => 'Enhance your lashes effortlessly with our precision-formulated mascara. Our volumizing and lengthening formula coats each lash, creating a dramatic yet natural look that lasts all day. Elevate your eye makeup routine with lashes that speak volumes.',
+                'price' => 23.99,
+            ],
+            [
+                'name' => 'Foundation',
+                'description' => 'Discover flawless radiance with our lightweight foundation, expertly crafted to provide seamless coverage. Infused with nourishing ingredients, it creates a smooth, natural finish that lasts throughout the day. Unveil a radiant complexion and embrace a foundation that enhances your natural beauty.',
+                'price' => 29.99,
+            ],
+            [
+                'name' => 'Blusher',
+                'description' => 'Blusher is a cosmetic product applied to the cheeks to add a rosy or flushed appearance, enhancing the natural color of the skin. Available in various formulations, including powder, cream, and liquid, blushers are used to create a youthful and radiant complexion. They come in a range of shades to suit different skin tones and makeup preferences.',
+                'price' => 9.99,
+            ],
+        ];
+
+        // Insert data into the products table
+        foreach ($products as $productData) {
+            Products::create($productData);
+        }
     }
 }
