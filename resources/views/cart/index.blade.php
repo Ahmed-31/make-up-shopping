71fa03e99538f5335bc7e30e7fadd66f0701520f
ahmed elzaky <ahmed_elzaky@outlook.com>
2023-12-25 08:47:23 +0200

    add cart page layout

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 <div class="container">
-
+    <h2>My Cart</h2>
+    <div class="row">
+        <div class="col-8 align-self-start">
+            @isset($user->orders)
+                @foreach ($user->orders as $order)
+                    <p>This is order {{ $order->name }}</p>
+                @endforeach
+            @else
+                <p>No Orders...</p>
+            @endisset
+        </div>
+        <div class="col-4 align-self-end">
+            One of three columns
+        </div>
+    </div>
 </div>
 @endsection
+
+
